ae64cf5019af9763c6a62d4a17f292d7dd3bdb1a
conf8o <41929738+conf8o@users.noreply.github.com>
2022-02-12 17:37:22 +0900

    start to implement CPU

---
+++ Sources/Hardware/CPU/CPU.swift
@@ -0,0 +1,6 @@
+public struct CPU {
+    // public mutating func access(_ inM: Bit16, _ instruction: Bit16, _ reset: Bit) 
+    // -> (outM: Bit16, writeM: Bit, addressM: Bit15, pc: Bit15) {
+    //     return 
+    // }
+}
