177cd5cfd36bc99ce49c28e33f03bbf98860815b
conf8o <41929738+conf8o@users.noreply.github.com>
2022-02-12 15:07:00 +0900

    implement ram with array for light compiling

--- Sources/Hardware/RAM/RAM.swift
+++ Sources/Hardware/RAM/RAM.swift
@@ -49,60 +49,61 @@ public struct RAM64 {
     }
 }
 
+// 以降memoriesをタプルで定義するとコンパイルに時間がかかりまくるのでArrayで定義している
 public struct RAM512 {
-    private var memories = (
+    private var memories = [
         RAM64(), RAM64(), RAM64(), RAM64(),
         RAM64(), RAM64(), RAM64(), RAM64()
-    )
+    ]
 
     public mutating func access(_ a: Bit16, _ address: Bit9, _ load: Bit) -> Bit16 {
         let address64 = address.0
         let addressInner = Bit6(address.1, address.2)
-        
+
         let loadBits = Bit.dmux8(load, address64)
         return Bit16.mux8(
-            memories.0.access(a, addressInner, loadBits.a),
-            memories.1.access(a, addressInner, loadBits.b),
-            memories.2.access(a, addressInner, loadBits.c),
-            memories.3.access(a, addressInner, loadBits.d),
-            memories.4.access(a, addressInner, loadBits.e),
-            memories.5.access(a, addressInner, loadBits.f),
-            memories.6.access(a, addressInner, loadBits.g),
-            memories.7.access(a, addressInner, loadBits.h),
+            memories[0].access(a, addressInner, loadBits.a),
+            memories[1].access(a, addressInner, loadBits.b),
+            memories[2].access(a, addressInner, loadBits.c),
+            memories[3].access(a, addressInner, loadBits.d),
+            memories[4].access(a, addressInner, loadBits.e),
+            memories[5].access(a, addressInner, loadBits.f),
+            memories[6].access(a, addressInner, loadBits.g),
+            memories[7].access(a, addressInner, loadBits.h),
             address64
         )
     }
 }
 
 public struct RAM4k {
-    private var memories = (
+    private var memories = [
         RAM512(), RAM512(), RAM512(), RAM512(),
         RAM512(), RAM512(), RAM512(), RAM512()
-    )
+    ]
 
     public mutating func access(_ a: Bit16, _ address: Bit12, _ load: Bit) -> Bit16 {
         let address512 = address.0
         let addressInner = Bit9(address.1, address.2, address.3)
-        
+
         let loadBits = Bit.dmux8(load, address512)
         return Bit16.mux8(
-            memories.0.access(a, addressInner, loadBits.a),
-            memories.1.access(a, addressInner, loadBits.b),
-            memories.2.access(a, addressInner, loadBits.c),
-            memories.3.access(a, addressInner, loadBits.d),
-            memories.4.access(a, addressInner, loadBits.e),
-            memories.5.access(a, addressInner, loadBits.f),
-            memories.6.access(a, addressInner, loadBits.g),
-            memories.7.access(a, addressInner, loadBits.h),
+            memories[0].access(a, addressInner, loadBits.a),
+            memories[1].access(a, addressInner, loadBits.b),
+            memories[2].access(a, addressInner, loadBits.c),
+            memories[3].access(a, addressInner, loadBits.d),
+            memories[4].access(a, addressInner, loadBits.e),
+            memories[5].access(a, addressInner, loadBits.f),
+            memories[6].access(a, addressInner, loadBits.g),
+            memories[7].access(a, addressInner, loadBits.h),
             address512
         )
     }
 }
 
 public struct RAM16k {
-    private var memories = (
+    private var memories = [
         RAM4k(), RAM4k(), RAM4k(), RAM4k()
-    )
+    ]
 
     public mutating func access(_ a: Bit16, _ address: Bit14, _ load: Bit) -> Bit16 {
         let address4k = Bit2(bits:(address.0, address.1))
@@ -110,10 +111,10 @@ public struct RAM16k {
 
         let loadBits = Bit.dmux4(load, address4k)
         return Bit16.mux4(
-            memories.0.access(a, addressInner, loadBits.a),
-            memories.1.access(a, addressInner, loadBits.b),
-            memories.2.access(a, addressInner, loadBits.c),
-            memories.3.access(a, addressInner, loadBits.d),
+            memories[0].access(a, addressInner, loadBits.a),
+            memories[1].access(a, addressInner, loadBits.b),
+            memories[2].access(a, addressInner, loadBits.c),
+            memories[3].access(a, addressInner, loadBits.d),
             address4k
         )
     }
--- Tests/HardwareTests/RAM/RAM512Tests.swift
+++ Tests/HardwareTests/RAM/RAM512Tests.swift
@@ -8,14 +8,16 @@ func bit9(
     _ d: Bit, _ e: Bit, _ f: Bit,
     _ g: Bit, _ h: Bit, _ i: Bit
 ) -> Bit9 {
-    Bit9(bit3s: (bit3(a, b, c), bit3(d, e, f), bit3(g, h, i)))
+    Bit9(bit3(a, b, c), bit3(d, e, f), bit3(g, h, i))
 }
 
 final class RAM512Tests: XCTestCase {
     func testRAM512() throws {
         var ram = RAM512()
-        
-        let first = Bit9.allOff /// 000 000 000 (最初の3bitsはRAM64のアドレス、二番目の3bitsはRAM8のアドレス、三番目の3bitsはregisterのアドレス)
+
+        let first = bit9(.off, .off, .off,
+                         .off, .off, .off,
+                         .off, .off, .off) /// 000 000 000 (最初の3bitsはRAM64のアドレス、二番目の3bitsはRAM8のアドレス、三番目の3bitsはregisterのアドレス)
         var bits = ram.access(bit16AllOn(), first, .on)  // 1111 1111 1111 1111 を0番目のレジスタに書き込み。読み込まれるのは初期値の0000 0000 0000 0000
         XCTAssertEqual(Bit16.allOff, bits)
         bits = ram.access(Bit16.allOff, first, .off) // Bit16.allOffは無視。保持していた1111 1111 1111 1111を取得
@@ -32,5 +34,5 @@ final class RAM512Tests: XCTestCase {
 
         bits = ram.access(Bit16.allOff, first, .off) // Bit16.allOffは無視。以前0番目に保持していた1111 1111 1111 1111を取得
         XCTAssertEqual(bit16AllOn(), bits)
-    }   
+    }
 }
